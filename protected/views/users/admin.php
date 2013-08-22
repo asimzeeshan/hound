5760e55751c3e7b89b8cecaa64b9c43996a6dde0
Asim Zeeshan <asim@techbytes.pk>
2013-08-22 22:25:29 +0500

    Some more tweaks to user/admin view

--- protected/views/users/admin.php
+++ protected/views/users/admin.php
@@ -49,11 +49,10 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		'first_name',
 		'last_name',
 		'username',
-		'password',
 		'email',
-		/*
 		'status',
 		'last_login',
+		/*
 		'created',
 		'created_by',
 		'modified',
