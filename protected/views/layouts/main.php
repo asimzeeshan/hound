0fe84be58b9f94b383853f30ae1c599416631a34
Asim Zeeshan <asim@techbytes.pk>
2013-08-22 22:24:05 +0500

    Updated menu

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -30,8 +30,10 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
+				//array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+				array('label'=>'Employees', 'url'=>array('/employees/admin'), 'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'System Users', 'url'=>array('/users/admin'), 'visible'=>!Yii::app()->user->isGuest),
+				//array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
