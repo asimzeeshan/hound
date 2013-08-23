ee4e3d41ab0f3ba9f06889a23d09889f7bcdba71
Asim Zeeshan <asim@techbytes.pk>
2013-08-23 13:51:46 +0500

    Generated Employees Model and CRUD via Gii

---
+++ protected/models/Employees.php
@@ -0,0 +1,136 @@
+<?php
+
+/**
+ * This is the model class for table "employees".
+ *
+ * The followings are the available columns in table 'employees':
+ * @property string $id
+ * @property string $emp_id
+ * @property string $name
+ * @property string $ip_address
+ * @property string $mac_address
+ * @property string $hostname
+ * @property string $description
+ * @property string $line_manager
+ * @property string $location
+ * @property string $hall
+ * @property string $opt
+ * @property string $created
+ * @property string $created_by
+ * @property string $modified
+ * @property string $modified_by
+ */
+class Employees extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'employees';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('name, ip_address, mac_address, hostname, description, line_manager, location, hall, opt, created_by, modified, modified_by', 'required'),
+			array('emp_id, name, ip_address, mac_address, hostname, description, line_manager, location, hall', 'length', 'max'=>255),
+			array('opt', 'length', 'max'=>256),
+			array('created_by, modified_by', 'length', 'max'=>11),
+			array('created', 'safe'),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, emp_id, name, ip_address, mac_address, hostname, description, line_manager, location, hall, opt, created, created_by, modified, modified_by', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'emp_id' => 'Emp',
+			'name' => 'Name',
+			'ip_address' => 'Ip Address',
+			'mac_address' => 'Mac Address',
+			'hostname' => 'Hostname',
+			'description' => 'Description',
+			'line_manager' => 'Line Manager',
+			'location' => 'Location',
+			'hall' => 'Hall',
+			'opt' => 'Opt',
+			'created' => 'Created',
+			'created_by' => 'Created By',
+			'modified' => 'Modified',
+			'modified_by' => 'Modified By',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id,true);
+		$criteria->compare('emp_id',$this->emp_id,true);
+		$criteria->compare('name',$this->name,true);
+		$criteria->compare('ip_address',$this->ip_address,true);
+		$criteria->compare('mac_address',$this->mac_address,true);
+		$criteria->compare('hostname',$this->hostname,true);
+		$criteria->compare('description',$this->description,true);
+		$criteria->compare('line_manager',$this->line_manager,true);
+		$criteria->compare('location',$this->location,true);
+		$criteria->compare('hall',$this->hall,true);
+		$criteria->compare('opt',$this->opt,true);
+		$criteria->compare('created',$this->created,true);
+		$criteria->compare('created_by',$this->created_by,true);
+		$criteria->compare('modified',$this->modified,true);
+		$criteria->compare('modified_by',$this->modified_by,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Employees the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
---
+++ protected/views/employees/_search.php
@@ -0,0 +1,95 @@
+<?php
+/* @var $this EmployeesController */
+/* @var $model Employees */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'id'); ?>
+		<?php echo $form->textField($model,'id',array('size'=>20,'maxlength'=>20)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'emp_id'); ?>
+		<?php echo $form->textField($model,'emp_id',array('size'=>60,'maxlength'=>255)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'name'); ?>
+		<?php echo $form->textField($model,'name',array('size'=>60,'maxlength'=>255)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'ip_address'); ?>
+		<?php echo $form->textField($model,'ip_address',array('size'=>60,'maxlength'=>255)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'mac_address'); ?>
+		<?php echo $form->textField($model,'mac_address',array('size'=>60,'maxlength'=>255)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'hostname'); ?>
+		<?php echo $form->textField($model,'hostname',array('size'=>60,'maxlength'=>255)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'description'); ?>
+		<?php echo $form->textField($model,'description',array('size'=>60,'maxlength'=>255)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'line_manager'); ?>
+		<?php echo $form->textField($model,'line_manager',array('size'=>60,'maxlength'=>255)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'location'); ?>
+		<?php echo $form->textField($model,'location',array('size'=>60,'maxlength'=>255)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'hall'); ?>
+		<?php echo $form->textField($model,'hall',array('size'=>60,'maxlength'=>255)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'opt'); ?>
+		<?php echo $form->textField($model,'opt',array('size'=>60,'maxlength'=>256)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'created'); ?>
+		<?php echo $form->textField($model,'created'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'created_by'); ?>
+		<?php echo $form->textField($model,'created_by',array('size'=>11,'maxlength'=>11)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'modified'); ?>
+		<?php echo $form->textField($model,'modified'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'modified_by'); ?>
+		<?php echo $form->textField($model,'modified_by',array('size'=>11,'maxlength'=>11)); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
