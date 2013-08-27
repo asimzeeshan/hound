e5eed3a116f1817044d4c82deea927e1eeccf6e9
Asim Zeeshan <asim@techbytes.pk>
2013-08-27 22:52:35 +0500

    Adding Whois controller

---
+++ protected/controllers/WhoisController.php
@@ -0,0 +1,74 @@
+<?php
+
+class WhoisController extends Controller
+{
+	/**
+	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+	 * using two-column layout. See 'protected/views/layouts/column2.php'.
+	 */
+	public $layout='//layouts/column1';
+
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+			'postOnly + delete', // we only allow deletion via POST request
+		);
+	}
+
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(
+			array('allow',  // allow all users to perform 'index' and 'view' actions
+				'actions'=>array('index','view'),
+				'users'=>array('@'),
+			),
+			array('allow', // allow authenticated user to perform 'create' and 'update' actions
+				'actions'=>array('create','update'),
+				'users'=>array('@'),
+			),
+			array('allow', // allow admin user to perform 'admin' and 'delete' actions
+				'actions'=>array('admin','delete'),
+				'users'=>array('@'),
+			),
+			array('deny',  // deny all users
+				'users'=>array('*'),
+			),
+		);
+	}
+
+	public function actionIndex()
+	{
+		$arraynew = array();
+		if( isset($_REQUEST['yt0']) ) { // if form is submitted (button id)
+			if($_REQUEST['search'] != '') { // if the search has some value
+				if($_REQUEST['type'] == 'emp_id') {
+					$model = new Employees;
+					$users_details = $model->searchByEmpID( $_REQUEST['search'] );
+					$arraynew = array();
+					foreach( $users_details as $details ) {
+						$arraynew[] = $details->attributes;
+					}
+				} elseif ($_REQUEST['type'] == 'ip_addr') {
+					$model = new Employees;
+					$users_details = $model->searchByIP( $_REQUEST['search'] );
+					$arraynew = array();
+					foreach( $users_details as $details ) {
+						$arraynew[] = $details->attributes;
+					}
+				}
+			} else {
+				Yii::app()->user->setFlash('whois_failed', "Please enter what you want to search!");
+			}
+		}
+		$this->render('index', array( 'userdata' => $arraynew ));
+	}
+}
---
+++ protected/views/whois/index.php
@@ -0,0 +1,117 @@
+<?php
+/* @var $this WhoisController */
+
+$this->breadcrumbs=array(
+	'Whois',
+);
+$selected = ((isset($_REQUEST['type']) && $_REQUEST['type'] == 'emp_id') ? 'emp_id' : 'ip_addr');
+?>
+<style type="text/css">
+.foundit td {
+	font-weight: bold;
+	color: #090;
+}
+.notfound td {
+	font-weight: bold;
+	color: #F90;
+}
+.notice {
+	background:#FBE3E4;
+	color:#8a1f11;
+	border-color:#FBC2C4;
+}
+</style>
+
+
+<h1>Whois</h1>
+
+<?php if(Yii::app()->user->hasFlash('whois_welcome')): ?>
+<div class="flash-success">
+	<?php echo Yii::app()->user->getFlash('whois_welcome'); ?>
+</div>
+<?php endif; ?>
+
+<?php if(Yii::app()->user->hasFlash('whois_failed')):?>
+    <div class="flash-error">
+        <?php echo Yii::app()->user->getFlash('whois_failed'); ?>
+    </div>
+<?php endif; ?>
+
+<div class="form">
+<?php echo CHtml::beginForm(); ?>
+  <div class="row">
+        <?php 
+        	echo CHtml::textField('search', '',
+			array('id'=>'search', 
+       		'width'=>100, 
+       		'maxlength'=>100));
+       	?>
+    </div>
+    <div class="row">
+    	<label>Search In :</label>
+        <?php echo CHtml::radioButtonList('type', $selected, array('ip_addr'=>'IP Address','emp_id'=>'Employee ID'),array(
+		    'labelOptions'=>array('style'=>'display:inline'), // add this code
+		    'separator'=>'&nbsp;&nbsp;&nbsp;&nbsp;',
+		)); ?>
+    </div>
+    <div class="row submit">
+        <?php echo CHtml::submitButton('Search'); ?>
+    </div>
+ 
+<?php echo CHtml::endForm(); ?>
+</div><!-- form -->
+<?php
+$emp_id = '';
+if(count($userdata) > 0) {
+	foreach ($userdata as $key => $value) {
+		if($value['emp_id'] != '')
+		{
+			$emp_id = $value['emp_id'];
+		}
+
+		if ($value['ip_address']==$_POST['search']) {
+			$class = " foundit"	;
+		} elseif ($value['emp_id']==$_POST['search']) {
+			$class = " foundit"	;
+		} else {
+			$class = " notfound";
+		}
+?>
+<div class="view">
+  <table cellspacing="2" cellpadding="3" align="center" class="<?php echo $class; ?>" border="1" >
+    <tr>
+      <td width="200">Employee ID</td>
+      <td><?php echo $value['emp_id']; ?> <?php if($value['emp_id'] != ''){ ?>(
+        <a href='<?php echo CController::createUrl('emailLogs/sendemail', array('emp_id' => $emp_id )); ?>'>REPORT?</a>
+      )<?php } else { ?><font class="notice">MISSING EMPLOYEE ID</font><?php } ?></td>
+    </tr>
+    <tr>
+      <td width="200">IP Address</td>
+      <td><?php echo $value['ip_address']; ?></td>
+    </tr>
+    <tr>
+      <td>Host Name</td>
+      <td><?php echo $value['hostname']; ?></td>
+    </tr>
+    <tr>
+      <td>Employee Name</td>
+      <td><?php echo $value['description']; ?></td>
+    </tr>
+    <tr>
+      <td>Manager Name</td>
+      <td><?php echo $value['line_manager']; ?></td>
+    </tr>
+    <tr>
+      <td>Employee Location</td>
+      <td><?php echo $value['location']; ?></td>
+    </tr>
+    <tr>
+      <td>Sitting Hall</td>
+      <td><?php echo $value['hall']; ?></td>
+    </tr>
+  </table>
+</div>
+<?php
+	}
+}
+?>
