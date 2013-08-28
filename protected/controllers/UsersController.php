d8c8f56938b9cfa8095e31b395eca9d73868ae52
Asim Zeeshan <asim@techbytes.pk>
2013-08-28 17:08:09 +0500

    Fixing another blunder
    $encrypted_pass should be saved before encrypted and sending

--- protected/controllers/UsersController.php
+++ protected/controllers/UsersController.php
@@ -69,20 +69,20 @@ class UsersController extends Controller
 
 		if(isset($_POST['Users']))
 		{
-			// ## Prepare the email
-			$et = new EmailTemplates;
-			$data = $et->getData(7);
-			
-			$search = array('{name}', '{username}', '{password}');
-			$replace = array($model->name(), $model->username, $model->password);
-			$body = str_ireplace($search, $replace, $data->body);
-			// ## ENDS preparing the email
-
-			// encrypting password
+			$unencryted_pass = $_POST['Users']['password'];
 			$_POST['Users']['password'] = md5($_POST['Users']['password']);
 			$model->attributes=$_POST['Users'];
 			if($model->save()) {
 				// send welcome email
+				$et = new EmailTemplates;
+				$data = $et->getData(7);
+				
+				// parse Body
+				$search = array('{name}', '{username}', '{password}');
+				$replace = array($model->name(), $model->username, $unencryted_pass);
+				$body = str_ireplace($search, $replace, $data->body);
+				// ends parse Body
+				
 				$this->sendMail(array(
 					'body'=> $body,
 					'address'=> $model->email,
@@ -91,7 +91,6 @@ class UsersController extends Controller
 					'subject' => $data->subject
 				));
 				
-				// now redirect
 				$this->redirect(array('view','id'=>$model->id));
 			}
 		}
