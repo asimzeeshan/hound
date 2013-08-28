687fb091c30657505403ece87552bbf6be537060
Asim Zeeshan <asim@techbytes.pk>
2013-08-28 16:34:30 +0500

    Updated menu

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -33,7 +33,9 @@
 				//array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Whois', 'url'=>array('/whois/'), 'visible'=>!Yii::app()->user->isGuest),
 				array('label'=>'Employees', 'url'=>array('/employees/admin'), 'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Email Logs', 'url'=>array('/emailLogs/admin'), 'visible'=>!Yii::app()->user->isGuest),
 				array('label'=>'System Users', 'url'=>array('/users/admin'), 'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Email Templates', 'url'=>array('/emailTemplates/admin'), 'visible'=>!Yii::app()->user->isGuest),
 				//array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
