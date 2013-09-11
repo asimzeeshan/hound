f7adde02c60939e7e5ad348ae82c3fa752a922c1
Asim Zeeshan <asim@techbytes.pk>
2013-09-11 22:32:40 +0500

    Updating output formatting for NextHRM command

--- protected/commands/NexthrmCommand.php
+++ protected/commands/NexthrmCommand.php
@@ -5,19 +5,20 @@ class NexthrmCommand extends CConsoleCommand {
 	}
 	
 	public function run($args) {
-		$employees = Devices::model()->findAll(array('condition'=>'emp_id!=""'));
-		foreach ($employees as $employee) {
-			$NextHRM = $this->_callNextHRM($employee->emp_id);
+		echo "Looking for all devices where emp_id is not NULL";
+		$devices = Devices::model()->findAll(array('condition'=>'emp_id!=""'));
+		foreach ($devices as $device) {
+			$NextHRM = $this->_callNextHRM($device->emp_id);
 			if ((string)$NextHRM->getSitting->status == "success") {
 				$data = json_decode($NextHRM->getSitting->response);
 				foreach ($data as $record) {
-					$this->_updateRecord($employee->emp_id, $record);
+					$this->_updateRecord($device->emp_id, $record);
 				}
 			} else {
-				echo "Server says bobo, the actual message is: ".(string)$NextHRM->getSitting->status;
+				echo "   => Server says bobo, the actual message is: ".(string)$NextHRM->getSitting->status;
 			}
-		// sleep for 1 second
-		sleep(1);
+		// usleep for 200 ms
+		usleep(200);
 		}
 
 	}
@@ -33,7 +34,7 @@ class NexthrmCommand extends CConsoleCommand {
 											'line_manager'	=> (string)$data->emp_manager_name, 
 											'location'		=> (string)$data->emp_location, 
 										), 'emp_id=:emp_id', array(':emp_id' => $emp_id));
-		echo "Updated all records for EmpID: ".$emp_id." successfully! \n";
+		echo " * Updated all records for EmpID: ".$emp_id." successfully! \n";
 	}
 }
 ?>
