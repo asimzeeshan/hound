a2eb38b2fd90b02a9b7c28efbb5a370b68b0ef29
Asim Sarwar <asim.sarwar@nxb.com.pk>
2014-05-02 15:44:18 +0500

    Configuration reverted

--- protected/config/console.php
+++ protected/config/console.php
@@ -20,10 +20,10 @@ return array(
 	// application components
 	'components'=>array(
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=abusereporter',
+			'connectionString' => 'mysql:host=localhost;dbname=abusereportr',
 			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
+			'username' => 'abusereportr',
+			'password' => 'T9wHWVGSeYBuMrSX',
 			'charset' => 'utf8',
 		),
 		'log'=>array(
