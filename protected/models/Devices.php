c241380272165e9cc8e75fdf92d01e35cf72d04c
Asim Sarwar <asim.sarwar@nxb.com.pk>
2014-05-05 17:43:24 +0500

    Update Without Emp Id List

--- protected/models/Devices.php
+++ protected/models/Devices.php
@@ -150,7 +150,7 @@ class Devices extends AZActiveRecord
 			  <th width='16%'>Ipaddar</th>
 			  <th width='16%'>Hostname</th>  ";
 		           foreach($Devices as $query){
-					  echo  $userLink = CHtml::link($query['name'],array("devices/update","id"=>$query['id']));
+					  echo  $userLink = CHtml::link("DFADSFASD",array("devices/update","id"=>$query['id']));
 			       $no_emp_id .= "<tr><td align='center'>".$userLink."</td>
 					      <td align='center'>". $query['mac_address']."</td>
 					      <td align='center'>". $query['ip_address']."</td>
