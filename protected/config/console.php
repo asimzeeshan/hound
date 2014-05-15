ddc61c58510dd661aed65070f2a827a3c3abb5bc
Asim Sarwar <asim.sarwar@nxb.com.pk>
2014-05-15 17:36:41 +0500

    ACL based changes

--- protected/config/console.php
+++ protected/config/console.php
@@ -19,20 +19,20 @@ return array(
 
 	// application components
 	'components'=>array(
-		'db'=>array(
+		/*'db'=>array(
 			'connectionString' => 'mysql:host=localhost;dbname=abusereportr',
 			'emulatePrepare' => true,
 			'username' => 'abusereportr',
 			'password' => 'T9wHWVGSeYBuMrSX',
 			'charset' => 'utf8',
-		),
-		/*'db'=>array(
+		),*/
+		'db'=>array(
 			'connectionString' => 'mysql:host=localhost;dbname=abusereporter',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
 			'charset' => 'utf8',
-		),*/
+		),
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
--- protected/models/Users.php
+++ protected/models/Users.php
@@ -189,7 +189,6 @@ class Users extends AZActiveRecord
 		));
 	}
 	
-	
 	/**
 	 * Returns the static model of the specified AR class.
 	 * Please note that you should have this exact method in all your CActiveRecord descendants!
